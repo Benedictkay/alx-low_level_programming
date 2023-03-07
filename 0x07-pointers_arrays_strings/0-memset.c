03eb975de058e4cff801f9b8cd6820378e530b0b
Benedictkay <benedictmaundu2000@gmail.com>
2023-03-07 04:45:57 -0800

    readme rename

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,22 @@
 #include "main.h"
+
 /**
- * _memset - fills memory with a constant byte.
- * @s: source string
- * @b: the contant byte for filling
- * @n: lenght of buffer
- * Return: new string.
- */
+*_memset - The _memset() function fills
+* the first n bytes of the memory area
+* pointed to by s with the constant byte b
+*@s:target
+*@b: constant byte
+*@n:number of byte
+*Return: returns new value of target
+*/
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
-
-	while (i < n)
+	while (n)
 	{
-		*(s + i) = b;
-		i++;
+		s[n - 1] = b;
+		n--;
 	}
 	return (s);
 }
+
